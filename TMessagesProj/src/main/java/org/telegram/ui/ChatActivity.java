4bdbe6a4ef2ffdc6f93ee891517027bde1cd76c6
ixiumu <ixiumu@users.noreply.github.com>
2025-11-12 15:29:10 +0800

    HideSponsoredMessages

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -24928,7 +24928,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private Pattern sponsoredUrlPattern;
     private MessageObject botSponsoredMessage;
     private void addSponsoredMessages(boolean animated) {
-        if (sponsoredMessagesAdded || chatMode != 0 || !ChatObject.isChannel(currentChat) && !UserObject.isBot(currentUser) || !forwardEndReached[0] || getUserConfig().isPremium() && getMessagesController().isSponsoredDisabled() || isReport()) {
+        /*if (sponsoredMessagesAdded || chatMode != 0 || !ChatObject.isChannel(currentChat) && !UserObject.isBot(currentUser) || !forwardEndReached[0] || getUserConfig().isPremium
+        () && getMessagesController().isSponsoredDisabled() || isReport()) {
             return;
         }
         MessagesController.SponsoredMessagesInfo res = getMessagesController().getSponsoredMessages(dialog_id);
@@ -24977,7 +24978,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 notPushedSponsoredMessages.clear();
             }
             processNewMessages(res.messages, false);
-        }
+        }*/
     }
 
     public void removeFromSponsored(MessageObject message) {
